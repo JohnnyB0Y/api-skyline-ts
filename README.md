3735aa21c040185503b6a4d6923e672649a3dcd2
JohnnyB0Y <ag860050872@163.com>
2021-03-21 12:33:40 +0800

    添加修改安全数据访问方法

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 ### 参考
 [Node.js and TypeScript Tutorial: Build a CRUD API](https://auth0.com/blog/node-js-and-typescript-tutorial-build-a-crud-api/)
+
+[TypeScript-Node-Starter](https://github.com/microsoft/TypeScript-Node-Starter)
+
+[Node-orm2: Object Relational Mapping](https://github.com/dresende/node-orm2)
--- src/common/safe_access/safe.ts
+++ src/common/safe_access/safe.ts
@@ -5,19 +5,19 @@
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
 
-function safeNumber(num: number, placeholder: any): any {
+function safeNumber(num: number, placeholder = 0): number {
   return safeValue(num, placeholder, val => {
     return typeof val == 'number';
   });
 }
 
-function safeString(str: string, placeholder: any): any {
+function safeString(str: string, placeholder = ''): string {
   return safeValue(str, placeholder, val => {
     return typeof val == 'string';
   });
 }
 
-function safeBoolean(bool: boolean, placeholder: any): any {
+function safeBoolean(bool: boolean, placeholder = false): boolean {
   return safeValue(bool, placeholder, val => {
     return typeof val == 'boolean';
   });
@@ -27,9 +27,25 @@ function safeValue(val: any, placeholder: any, condition: (val: any) => boolean)
   return condition(val) ? val : placeholder;
 }
 
+function safeStringEqual(val1: string, val2: string): boolean {
+  if (typeof val1 != 'string' || typeof val2 != 'string') {
+    return false;
+  }
+  return val1 === val2
+}
+
+function safeNumberEqual(val1: number, val2: number): boolean {
+  if (typeof val1 != 'number' || typeof val2 != 'number') {
+    return false;
+  }
+  return val1 === val2
+}
+
 export const Safe = {
   number: safeNumber,
   string: safeString,
   boolean: safeBoolean,
   condition: safeValue,
+  numberEqual: safeNumberEqual,
+  stringEqual: safeStringEqual,
 }
